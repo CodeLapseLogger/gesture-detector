b277fe1f7074ffdbb955a9864de10cb3ab1d5d35
Surya T N <suryatej.nim@yahoo.com>
2022-12-17 20:32:34 +0530

    Fixed the code in toggleGestureDetectionMode method to properly update the state of isDetectingGesture variable. Implemented GestureDetectorBgContainer, ToggleGestureDetectionButton styled components to render background container and toggle button JSX elements in GestureDetector component.

--- src/components/GestureDetector/index.js
+++ src/components/GestureDetector/index.js
@@ -1,14 +1,68 @@
-import { Component, useRef } from "react";
+import { useState, useRef } from "react";
 import * as tf from "@tensorflow/tfjs";
 import * as handpose from "@tensorflow-models/handpose";
 import Webcam from "react-webcam";
 
-export default class GestureDetector extends Component {
-  state = {
-    isDetectingGesture: false,
-  };
+import {
+  GestureDetectorBgContainer,
+  ToggleGestureDetectionButton,
+} from "./styledComponents";
 
-  render() {
-    return <h1> Gesture Detector </h1>;
-  }
-}
+const GestureDetector = (props) => {
+  const webCamRef = useRef(null);
+  const canvasRef = useRef(null);
+
+  const [isDetectingGesture, setGestureDetectionState] = useState(false);
+
+  const toggleGestureDetectionMode = () =>
+    setGestureDetectionState(!isDetectingGesture);
+
+  return (
+    <GestureDetectorBgContainer>
+      {isDetectingGesture && (
+        <>
+          <Webcam
+            ref={webCamRef}
+            style={{
+              position: "absolute",
+              marginLeft: "auto",
+              marginRight: "auto",
+              left: 0,
+              right: 0,
+              textAlign: "center",
+              zindex: 1,
+              width: "60vw",
+              height: "70vh",
+            }}
+          />
+
+          <canvas
+            ref={canvasRef}
+            style={{
+              position: "absolute",
+              marginLeft: "auto",
+              marginRight: "auto",
+              left: 0,
+              right: 0,
+              textAlign: "center",
+              zindex: 1,
+              width: "60vw",
+              height: "70vh",
+            }}
+          />
+        </>
+      )}
+
+      <ToggleGestureDetectionButton
+        type="button"
+        onClick={toggleGestureDetectionMode}
+      >
+        {isDetectingGesture
+          ? "Stop Gesture Detection"
+          : "Start Gesture Detection"}
+      </ToggleGestureDetectionButton>
+    </GestureDetectorBgContainer>
+  );
+};
+
+export default GestureDetector;
---
+++ src/components/GestureDetector/styledComponents.js
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export const GestureDetectorBgContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+  height: 100%;
+  padding: 2rem;
+  background-color: ${(props) => (props.isDarkTheme ? "#191a1c" : "#ffffff")};
+`;
+
+export const ToggleGestureDetectionButton = styled.button`
+  background-color: ${(props) => (props.isDarkTheme ? "#76b0cc" : "#1a80f4")};
+  color: #ffffff;
+  padding: 1rem 1.5rem;
+  border: none;
+  border-radius: 0.6rem;
+  cursor: pointer;
+  font-size: 1.5rem;
+  font-family: "Roboto";
+  font-weight: 550;
+`;
