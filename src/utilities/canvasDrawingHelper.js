7d8d65f50f55fb2a011b7b935b0dbe3377b64303
Surya T N <suryatej.nim@yahoo.com>
2022-12-18 11:13:08 +0530

    Implemented code in drawHandMesh() method to draw lines of canvas connecting the finger landmarks in respective fingers

--- src/utilities/canvasDrawingHelper.js
+++ src/utilities/canvasDrawingHelper.js
@@ -17,6 +17,45 @@ export const drawHandMesh = (detectedHandData, canvas2DContext) => {
   detectedHandData.forEach((detectedHandDataEntry) => {
     const detectedHandLandmarkData = detectedHandDataEntry.landmarks; // array with 21 entries
 
+    // Connect finger landmarks with lines between
+    // respective pairs of landmarks on canvas
+    const fingerNames = Object.keys(fingerJointLandmarkMapping);
+
+    for (let fingerName of fingerNames) {
+      // Connect each pair of current finger landmarks
+      const currentFingerLandmarks = fingerJointLandmarkMapping[fingerName];
+      const currentFingerLandmarkCount = currentFingerLandmarks.length;
+
+      for (
+        let currentFingerLandmarkIndex = 0;
+        currentFingerLandmarkIndex < currentFingerLandmarkCount - 1;
+        currentFingerLandmarkIndex++
+      ) {
+        const firstLandmarkInThePair =
+          currentFingerLandmarks[currentFingerLandmarkIndex];
+        const secondLandmarkInThePair =
+          currentFingerLandmarks[currentFingerLandmarkIndex];
+
+        // Connect the two landmarks with a canvas line path
+        canvas2DContext.beginPath();
+        // Move to x, y co-ordinates of first landmark position in the pair
+        canvas2DContext.moveTo(
+          detectedHandLandmarkData[firstLandmarkInThePair][0],
+          detectedHandLandmarkData[firstLandmarkInThePair][1]
+        );
+        // Draw line to x, y co-ordinates of second landmark position from the first in the pair
+        canvas2DContext.lineTo(
+          detectedHandLandmarkData[secondLandmarkInThePair][0],
+          detectedHandLandmarkData[secondLandmarkInThePair][1]
+        );
+
+        // Style the line
+        canvas2DContext.strokeStyle = "plum";
+        canvas2DContext.lineWidth = 4;
+        canvas2DContext.stroke();
+      }
+    }
+
     // Highlight each landmark in the video feed by
     // drawing it in the input canvas context.
     for (
